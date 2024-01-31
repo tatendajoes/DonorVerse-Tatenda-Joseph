c917fc0e859355c5509cc7f60c9d4781af603f78
tatendajoes <142287523+tatendajoes@users.noreply.github.com>
2024-01-31 06:01:53 +0000

    Added Select Case to main menu

---
+++ .ccls-cache/@home@runner@DonorVerse-Tatenda-Joseph/data_structures.cpp
@@ -0,0 +1,9 @@
+namespace definitions {
+enum class Option : int {
+  // Opt is shorthand for Option
+  Opt1 = 1,
+  Opt2 = 2,
+  Opt3 = 3,
+  Opt4 = 4
+};
+}
--- .ccls-cache/@home@runner@DonorVerse-Tatenda-Joseph/main.cpp
+++ .ccls-cache/@home@runner@DonorVerse-Tatenda-Joseph/main.cpp
@@ -1,12 +1,28 @@
 // DonorVerse main page
 // This module handles the interface between the User and the application. 
-#include <iostream>
-using namespace std;
 
-// In this main function a loop is implemented which continues to run until a vaild Menu input is inserted
-int main() {
-  int sel=0;
-  while (sel<1 || sel>4){
+//Importing required modules and Libraries
+#include <iostream> //This is the iostream built in Library for C++ input output functions
+#include "data_structures.cpp"
+
+// Importing namespaces in various modules into module
+using namespace std;
+using namespace definitions;
+
+
+void add_donor(){
+  cout<< "Something about donors";
+}
+void search_donor(){
+  cout<< "Something about searching donors";
+}
+void modify_donors(){
+  cout<< "Something about modifying donors";
+}
+int main() { //TJ – this introduces the first function the compiler will place in the compiled instructions
+
+  int sel=4;
+  while (sel>0 && sel<5){
     cout<< "MINORITY EMPOWERMENT  \n THROUGH TECHNOLOGY \n \n";
     cout << "Main Menu \n \n";
     cout << "1. Add Donor or Donation \n \n";
@@ -14,6 +30,24 @@ int main() {
     cout << "3. Modify Donors \n \n";
     cout << "4. Exit \n \n";
     cout << "Select: ";
-    cin>> sel;
+    cin >> sel;
+    switch(sel){
+      case 1:
+        sel=0;
+      add_donor();
+      case 2:
+        sel=0;
+      search_donor();
+      case 3:
+        sel=0;
+      modify_donors();
+      case 4:
+      sel=0;
+      cout<< "\n Exiting......... \n";
+      cout << "Goodbye";
+      default:
+      cout << "Something went wrong. Try again!";
+      sel=4;
+    }
+    }  
   }
-}
